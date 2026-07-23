cbb4457d20a047ea5a8c2ae7e93e09451e1b8fd8
GuillermoCarrero <www.gcarrerollovera.world@gmail.com>
2026-07-23 15:55:22 -0300

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from './components/Header';
+
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <div>
     
 <BrowserRouter>
+
+<Header/>
 
 <Routes>
   
